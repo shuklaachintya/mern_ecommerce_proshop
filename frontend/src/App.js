c6f260ce109a4ab7f282bdd1e18ba8e20595e26c
Achintya Shukla <achintshukla@gmail.com>
2023-12-23 15:29:40 +0530

    implemented React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
 
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
