ee49e639a058177cfbddf02b6ad853e17fae0170
Achintya Shukla <achintshukla@gmail.com>
2025-03-03 02:01:04 +0530

    checked uploading of new product image, working fine, attempt 2

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,8 @@ app.get("/api/config/paypal", (req, res) =>
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static("/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  //app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
